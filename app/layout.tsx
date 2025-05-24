6e918c418f1d9bf72a23a5479acf58eda57abae4
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-24 02:49:59 +0000

    chore: update dependencies and enhance Tailwind CSS configuration
    - Added new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, tailwind-merge, and tailwindcss-animate.
    - Updated devDependencies: @eslint/eslintrc, prettier, prettier-plugin-tailwindcss, and typescript.
    - Enhanced Tailwind CSS configuration with dark mode support and extended color palette.
    - Added custom border radius values and integrated tailwindcss-animate plugin.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -24,10 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <div className="flex h-screen flex-col">
+          <main className="flex-1 wrapper">{children}</main>
+        </div>
       </body>
     </html>
   );
