bd5717dd7b9a57bc42c1b86810e71ff79c4af857
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-26 00:45:10 +0000

    feat: add Footer component with current year and app name

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import { APP_NAME } from "@/lib/constants";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="border-t">
+      <div className="flex-center p-5">
+        {currentYear} {APP_NAME}. All Rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
