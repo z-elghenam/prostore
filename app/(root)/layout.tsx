486a414a509bf65c4dc6a71c2c41c29b84f3a9ec
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-26 00:02:51 +0000

    feat: add RootLayout component for main application structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+export default function RootLayout({children}: {children: React.ReactNode}){
+
+  return (
+    <div className='flex h-screen flex-col'>
+      <main className='flex-1 wrapper'>{children}</main>
+    </div>
+  );
+}
