1a89a4a6aa12fab80bd35c4fc4b98bfa21ebf166
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-28 00:57:24 +0000

    feat: implement HomePage component to display latest products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 const HomePage = () => {
-  return <Button>Button</Button>;
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  );
 };
-
 export default HomePage;
