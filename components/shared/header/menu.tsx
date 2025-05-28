3d00ea72bab425fe4326df0d5db9d3a4971489d7
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-28 00:58:15 +0000

    feat: add Menu component for navigation with responsive design

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,59 @@
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Link from "next/link";
+import ModeToggle from "./mode-toggle";
+
+const Menu = () => {
+  return (
+    <>
+      <div className="flex justify-end gap-3">
+        <nav className="hidden w-full max-w-xs gap-1 md:flex">
+          <ModeToggle />
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart />
+              Cart
+            </Link>
+          </Button>
+          <Button asChild>
+            <Link href="/sign-in">
+              <UserIcon />
+              Sign In
+            </Link>
+          </Button>
+        </nav>
+        <nav className="md:hidden">
+          <Sheet>
+            <SheetTrigger className="align-middle">
+              <EllipsisVertical />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col items-start">
+              <SheetTitle>Menu</SheetTitle>
+              <ModeToggle />
+              <Button asChild variant="ghost">
+                <Link href="/cart">
+                  <ShoppingCart />
+                  Cart
+                </Link>
+              </Button>
+              <Button asChild>
+                <Link href="/sign-in">
+                  <UserIcon />
+                  Sign In
+                </Link>
+              </Button>
+            </SheetContent>
+          </Sheet>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Menu;
