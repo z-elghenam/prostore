32e212666c3121159043fc9a6ccb9a5f88b3a583
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-28 00:57:37 +0000

    feat: add ProductList component to display a list of products with optional title and limit

---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,33 @@
+import ProductCard from "./product-card";
+
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any[];
+  title?: string;
+  limit?: number;
+}) => {
+  // Apply limit if provided, otherwise show all products
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      {limitedData.length > 0 ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {limitedData.map((product: any) => (
+            <ProductCard key={product.slug} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
