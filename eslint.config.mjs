f022e631a1cd935958eebcbd491785fa498b1028
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-31 01:10:28 +0000

    refactor: comment out unused simple-import-sort plugin configuration in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
+// import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,20 +12,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-    },
-    rules: {
-      "simple-import-sort/imports": [
-        "error",
-        {
-          groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]],
-        },
-      ],
-      "simple-import-sort/exports": "error",
-    },
-  },
+  // {
+  //   plugins: {
+  //     "simple-import-sort": simpleImportSort,
+  //   },
+  //   rules: {
+  //     "simple-import-sort/imports": [
+  //       "error",
+  //       {
+  //         groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]],
+  //       },
+  //     ],
+  //     "simple-import-sort/exports": "error",
+  //   },
+  // },
 ];
 
 export default eslintConfig;
