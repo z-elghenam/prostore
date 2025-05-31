181b0f312447200aebbe3c1b48d87198da1b10c6
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-31 00:36:23 +0000

    fix: format import statement for consistency

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { type ClassValue,clsx } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,4 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
+}
+
+// 49.9 becomes “49.90”
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
