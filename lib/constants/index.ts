f8b8d70d9051f61c8444cee08e4d9297d0f86727
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-06-09 14:03:33 +0000

    feat: add default sign-in values for authentication

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,3 +7,8 @@ export const SERVER_URL =
 
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+
+export const signInDefaultValues = {
+  email: "admin@example.com",
+  password: "123456",
+};
