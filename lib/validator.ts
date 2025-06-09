436d7ff7a1b1ff255584c092a7406b1aaf3249c3
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-06-09 17:34:44 +0000

    feat: add signUpFormSchema for user registration validation

--- lib/validator.ts
+++ lib/validator.ts
@@ -26,3 +26,18 @@ export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(3, "Password must be at least 3 characters"),
 });
+
+// Schema for signing up a user
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters"),
+    email: z.string().min(3, "Email must be at least 3 characters"),
+    password: z.string().min(3, "Password must be at least 3 characters"),
+    confirmPassword: z
+      .string()
+      .min(3, "Confirm password must be at least 3 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
