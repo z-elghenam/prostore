0f16ed1e94c5b09dcef659b11c704d2c0ee9802d
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-06-17 00:38:12 +0000

    feat: create Sign In form with validation and error handling

--- app/(root)/(auth)/sign-in/signin-form.tsx
+++ app/(root)/(auth)/sign-in/signin-form.tsx
@@ -2,14 +2,17 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { signUpDefaultValues } from "@/lib/constants";
+import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
 import { useActionState } from "react";
 import { useFormStatus } from "react-dom";
-import { signUp } from "@/lib/actions/user.actions";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { useSearchParams } from "next/navigation";
 
-const SignUpForm = () => {
-  const [data, action] = useActionState(signUp, {
+const SignInForm = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+  const [data, action] = useActionState(signInWithCredentials, {
     message: "",
     success: false,
   });
@@ -17,52 +20,32 @@ const SignUpForm = () => {
   return (
     <form action={action}>
       <div className="space-y-6">
-        <div className="space-y-2">
-          <Label htmlFor="name">Name</Label>
-          <Input
-            id="name"
-            name="name"
-            required
-            type="name"
-            placeholder={signUpDefaultValues.name}
-            autoComplete="name"
-          />
-        </div>
-        <div className="space-y-2">
+        <input type="hidden" name="callback" value={callbackUrl} />
+
+        <div>
           <Label htmlFor="email">Email</Label>
           <Input
             id="email"
             name="email"
             required
             type="email"
-            placeholder={signUpDefaultValues.email}
+            placeholder={signInDefaultValues.email}
             autoComplete="email"
           />
         </div>
-        <div className="space-y-2">
+        <div>
           <Label htmlFor="password">Password</Label>
           <Input
             id="password"
             name="password"
             required
             type="password"
-            placeholder={signUpDefaultValues.password}
-            autoComplete="current-password"
-          />
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="confirmPassword">Confirm Password</Label>
-          <Input
-            id="confirmPassword"
-            name="confirmPassword"
-            required
-            type="password"
-            placeholder={signUpDefaultValues.confirmPassword}
+            placeholder={signInDefaultValues.password}
             autoComplete="current-password"
           />
         </div>
         <div>
-          <SignUpButton />
+          <SignInButton />
 
           {data && !data.success && (
             <div className="text-center text-destructive">{data.message}</div>
@@ -70,9 +53,9 @@ const SignUpForm = () => {
         </div>
 
         <div className="text-center text-sm text-muted-foreground underline">
-          Have an account?{" "}
-          <Link target="_self" className="link" href="/sign-in">
-            Sign In
+          Don&apos;t have an account?{" "}
+          <Link target="_self" className="link" href="/sign-up">
+            Sign Up
           </Link>
         </div>
       </div>
@@ -80,13 +63,13 @@ const SignUpForm = () => {
   );
 };
 
-const SignUpButton = () => {
+const SignInButton = () => {
   const { pending } = useFormStatus();
   return (
     <Button disabled={pending} className="w-full" variant="default">
-      {pending ? "Sign up..." : "Sign Up"}
+      {pending ? "Signing In..." : "Sign In with credentials"}
     </Button>
   );
 };
 
-export default SignUpForm;
+export default SignInForm;
